caa3decaf564ad1203a292ad9772c980329dcfdd
tpmccallum <tpmccallum@users.noreply.github.com>
2015-03-06 07:45:16 +1000

    Update driver.py

--- driver.py
+++ driver.py
@@ -8,22 +8,19 @@ compressedFile = "content" + dateOfBackup + ".tar.gz"
 tar = tarfile.open(compressedFile, "w:gz")
 tar.add("/home/bitnami/apps/wordpress/htdocs/wp-content/")
 tar.close()
-#add the content to dropbox
+add the content to dropbox
 client = dropbox.client.DropboxClient("asdf")
 print 'linked account: ', client.account_info()
 
 f = open(compressedFile, 'rb')
 response = client.put_file(compressedFile, f)
 print 'uploaded: ', response
-#clean up
+clean up
 os.remove(compressedFile)
 
 #back up the database
-DB_HOST = "localhost"
-DB_USER = "asdf"
-DB_USER_PASSWORD = "asdf"
-DB_NAME = "asdf"
-os.system("mysqldump --add-drop-table -c -u " + str(DB_USER) + " -p " + str(DB_USER_PASSWORD) + str(DB_NAME) + ";" > "asdf" + ".sql")
+sqlCommand = """mysqldump --add-drop-table > database.sql"""
+os.system(sqlCommand)
 compressedFile = "database" + dateOfBackup + ".tar.gz"
 tar = tarfile.open(compressedFile, "w:gz")
 tar.add("asdf" + ".sql")
