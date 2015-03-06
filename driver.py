ec4c750024d7750287ac790b4da09d88c99c15f0
tpmccallum <tpmccallum@users.noreply.github.com>
2015-03-06 17:00:16 +1000

    Adding function for the Dropbox upload

--- driver.py
+++ driver.py
@@ -2,6 +2,16 @@ import dropbox
 import tarfile
 import os
 import time
+
+#functions
+def addContentToDropbox(compressedFile):
+  client = dropbox.client.DropboxClient("asdf")
+  print 'linked account: ', client.account_info()
+  f = open(compressedFile, 'rb')
+  response = client.put_file(compressedFile, f)
+  print 'uploaded: ', response
+
+
 #back up the content
 dateOfBackup = time.strftime("%Y-%m-%d") 
 compressedFile = "content" + dateOfBackup + ".tar.gz"
@@ -9,12 +19,7 @@ tar = tarfile.open(compressedFile, "w:gz")
 tar.add("/home/bitnami/apps/wordpress/htdocs/wp-content/")
 tar.close()
 #add the content to dropbox
-client = dropbox.client.DropboxClient("asdf")
-print 'linked account: ', client.account_info()
-
-f = open(compressedFile, 'rb')
-response = client.put_file(compressedFile, f)
-print 'uploaded: ', response
+addContentToDropbox(compressedFile)
 #clean up
 os.remove(compressedFile)
 
@@ -26,11 +31,6 @@ tar = tarfile.open(compressedFile, "w:gz")
 tar.add("asdf" + ".sql")
 tar.close()
 #add the database to dropbox
-client = dropbox.client.DropboxClient("asdf")
-print 'linked account: ', client.account_info()
-
-f = open(compressedFile, 'rb')
-response = client.put_file(compressedFile, f)
-print 'uploaded: ', response
+addContentToDropbox(compressedFile)
 #clean up
 os.remove(compressedFile)
